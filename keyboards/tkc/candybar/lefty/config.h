5e2381ffcdb4339c3b2a7b9b63dfc32110311ebe
Tobey <12691785+tobey-thai@users.noreply.github.com>
2020-11-15 17:13:03 -0800

    Add TAPPING_FORCE_HOLD for candybar/lefty

--- keyboards/tkc/candybar/lefty/config.h
+++ keyboards/tkc/candybar/lefty/config.h
@@ -109,3 +109,4 @@
 // #define WS2812_DMA_STREAM STM32_DMA1_STREAM2  // DMA stream for TIMx_UP (look up in reference manual under DMA Channel selection)
 //#define WS2812_DMA_CHANNEL 7                  // DMA channel for TIMx_UP
 //#define WS2812_EXTERNAL_PULLUP
+#define TAPPING_FORCE_HOLD
